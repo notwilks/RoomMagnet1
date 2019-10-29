5ed71c650a7437d4d3fa2a2774a829b45667b67f
landau <landauns@dukes.jmu.edu>
2019-10-29 18:46:25 -0400

    comment on home page .cs

--- RoomMagnet1/HomePage.aspx.cs
+++ RoomMagnet1/HomePage.aspx.cs
@@ -9,7 +9,7 @@ public partial class HomePage : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //I really hope this works
     }
 
     protected void SearchButton_Click(object sender, EventArgs e)
