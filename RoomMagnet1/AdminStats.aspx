aa9f8693fafcb544e9d2f367f609f355fca7326a
landau <landauns@dukes.jmu.edu>
2019-11-18 14:54:52 -0500

    Admin Dash working with test tableau info

---
+++ RoomMagnet1/AdminStats.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/RoomMagnet.master" AutoEventWireup="true" CodeFile="AdminStats.aspx.cs" Inherits="AdminStats" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+        <!doctype html>
+    <script type="text/javascript">
+        function ShowPopup(body) {
+            $("#exampleModal .modal-body").html(body)
+            $("#exampleModal").modal("show");
+        }
+
+        function ShowPopupTenant(body) {
+            $("#tenantModal .modal-body").html(body)
+            $("#tenantModal").modal("show");
+        }
+    </script>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Room Magnet">
+<meta name="keywords" content="room magnet, roommate, housing, matchmaking, house, apartment, living arrangement">
+<meta name="author" content="Room Magnet">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Dashboard</title>
+
+<!-- Bootstrap v4 -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="screen">
+<!-- custom css -->
+<link href="css/custom.css" rel="stylesheet" type="text/css" media="screen">
+<link rel="shortcut icon" href="images/logo-03.png" type="image/x-icon"/>
+<link href="https://fonts.googleapis.com/css?family=Oswald:400|Raleway:300&display=swap" rel="stylesheet">    
+</head>
+    
+<body>
+<form runat="server">     
+<div id="containerDiv" class="container">
+
+  <div class="row" style="margin-top: 7rem;">
+      <h1><asp:Label ID="admindash" runat="server" Text="Admin Dashboard"></asp:Label></h1>
+    </div><!-- end div row -->  
+   
+</div>
+    <div class="row" style="margin-right: 4rem; margin-left: 4rem;">
+    <div class="col-md-1">
+
+      </div>
+
+    <div class="col-md-10" style="margin-bottom: 2rem; margin-top: 2rem">
+        
+        
+         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                </button>
+                
+             <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="">
+                            <a class="nav-link" href="AdminDashboard.aspx">Search Users</a>
+                        </li>
+                        <li class="active">
+                            <a class="nav-link" href="AdminStats.aspx">Site Statistics</a>
+                        </li>
+                        <li class="">
+                            <a class="nav-link" href="CreateAdmin.aspx">Create New Admin</a>
+                        </li>
+                    </ul>
+            </div>
+        </nav>
+
+      </div>
+
+      <div class="col-md-1">
+
+      </div>
+ 
+        </div>
+
+    <div class="container">
+        <div class='tableauPlaceholder' id='viz1574106484886' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Te&#47;TestTableau_15741064639060&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='TestTableau_15741064639060&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Te&#47;TestTableau_15741064639060&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1574106484886');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+    </div>
+
+    </form>
+    </body>
+    </html>
+</asp:Content>
+
---
+++ RoomMagnet1/CreateAdmin.aspx.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+
+public partial class CreateAdmin : System.Web.UI.Page
+{
+    SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["RoomMagnetAWS"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void NextButton_Click(object sender, EventArgs e)
+    {
+        sc.Open();
+
+        System.Data.SqlClient.SqlCommand insert = new System.Data.SqlClient.SqlCommand();
+        insert.Connection = sc;
+
+        System.Data.SqlClient.SqlCommand setPass = new System.Data.SqlClient.SqlCommand();
+        setPass.Connection = sc;
+
+        System.Data.SqlClient.SqlCommand select = new System.Data.SqlClient.SqlCommand();
+        select.Connection = sc;
+
+        try
+        {
+            if (EmailBox.Text.Contains("@") && EmailBox.Text.Length > 0 && ConfirmEmailBox.Text.Length > 0)
+            {
+                EmailErrorLbl.Text = "";
+
+                if (EmailBox.Text == ConfirmEmailBox.Text)
+                {
+                    EmailErrorLbl.Text = "";
+                    ConfirmEmailErrorLbl.Text = "";
+                    PasswordErrorLbl.Text = "";
+                    ConfirmPasswordErrorLbl.Text = "";
+
+                    if (PasswordBox.Text.Length >= 8 && ConfirmPasswordBox.Text.Length >= 8)
+                    {
+                        PasswordErrorLbl.Text = "";
+                        ConfirmPasswordErrorLbl.Text = "";
+
+                        if (PasswordBox.Text.Contains("!") || PasswordBox.Text.Contains("@") || PasswordBox.Text.Contains("#") || PasswordBox.Text.Contains("$") || PasswordBox.Text.Contains("%")
+                            || PasswordBox.Text.Contains("^") || PasswordBox.Text.Contains("&") || PasswordBox.Text.Contains("*") || PasswordBox.Text.Contains("(") || PasswordBox.Text.Contains(")")
+                            || PasswordBox.Text.Contains("-") || PasswordBox.Text.Contains("_") || PasswordBox.Text.Contains("+") || PasswordBox.Text.Contains("="))
+                        {
+                            PasswordErrorLbl.Text = "";
+                            ConfirmPasswordErrorLbl.Text = "";
+
+                            if (PasswordBox.Text.Contains("0") || PasswordBox.Text.Contains("1") || PasswordBox.Text.Contains("2") || PasswordBox.Text.Contains("3") || PasswordBox.Text.Contains("4")
+                                || PasswordBox.Text.Contains("5") || PasswordBox.Text.Contains("6") || PasswordBox.Text.Contains("7") || PasswordBox.Text.Contains("8") || PasswordBox.Text.Contains("9"))
+                            {
+                                PasswordErrorLbl.Text = "";
+                                ConfirmPasswordErrorLbl.Text = "";
+
+                                if (PasswordBox.Text == ConfirmPasswordBox.Text)
+                                {
+                                    PasswordErrorLbl.Text = "";
+                                    ConfirmPasswordErrorLbl.Text = "";
+
+                                    setPass.CommandText = "INSERT INTO [dbo].[Passwords] (email, password, userType, lastUpdated, lastUpdatedBy) VALUES " +
+                                                    "(@email, @password, @userType, @lastUpdated, @lastUpdatedBy)";
+
+                                    setPass.Parameters.Add(new SqlParameter("@email", EmailBox.Text));
+                                    setPass.Parameters.Add(new SqlParameter("@password", PasswordHash.HashPassword(PasswordBox.Text)));
+                                    setPass.Parameters.Add(new SqlParameter("@userType", "A"));
+                                    setPass.Parameters.Add(new SqlParameter("@lastUpdatedBy", "Joe Muia"));
+                                    setPass.Parameters.Add(new SqlParameter("@lastUpdated", DateTime.Now));
+
+                                    setPass.ExecuteNonQuery();
+
+                                    OutputLabel.Text = "Account successfully created.";
+                                    EmailBox.Text = "";
+                                    ConfirmEmailBox.Text = "";
+                                    PasswordBox.Text = "";
+                                    ConfirmPasswordBox.Text = "";
+                                }
+                                else
+                                {
+                                    PasswordErrorLbl.Text = "Please make sure both passwords match.";
+                                    ConfirmPasswordErrorLbl.Text = "Please make sure both passwords match.";
+                                }
+                            }
+                            else
+                            {
+                                PasswordErrorLbl.Text = "Passsword must contain a number.";
+
+                                if (ConfirmPasswordBox.Text != PasswordBox.Text)
+                                {
+                                    ConfirmPasswordErrorLbl.Text = "Please make sure passwords match.";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            PasswordErrorLbl.Text = "Password must contain a special character.";
+
+                            if (ConfirmPasswordBox.Text != PasswordBox.Text)
+                            {
+                                ConfirmPasswordErrorLbl.Text = "Please make sure passwords match.";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        PasswordErrorLbl.Text = "Password must be at least 8 characters long.";
+
+                        if (ConfirmPasswordBox.Text != PasswordBox.Text)
+                        {
+                            ConfirmPasswordErrorLbl.Text = "Please make sure passwords match.";
+                        }
+                    }
+                }
+                else
+                {
+                    EmailErrorLbl.Text = "Please make sure both emails match.";
+                    ConfirmEmailErrorLbl.Text = "Please make sure both emails match.";
+
+                    if (PasswordBox.Text == "")
+                    {
+                        PasswordErrorLbl.Text = "Please enter a valid password.";
+                    }
+
+                    if (ConfirmPasswordErrorLbl.Text == "")
+                    {
+                        ConfirmPasswordErrorLbl.Text = "Please enter a valid password.";
+                    }
+                }
+            }
+            else
+            {
+                EmailErrorLbl.Text = "Please enter a valid email address.";
+
+                if (ConfirmEmailBox.Text == "")
+                {
+                    ConfirmEmailErrorLbl.Text = "Please enter a valid email address.";
+                }
+
+                if (PasswordBox.Text == "")
+                {
+                    PasswordErrorLbl.Text = "Please enter a valid password.";
+                }
+
+                if (ConfirmPasswordBox.Text == "")
+                {
+                    ConfirmPasswordErrorLbl.Text = "Please enter a valid password.";
+                }
+            }
+        }
+
+        catch (Exception ex)
+        {
+            //OutputLabel.Text = "An account with this email already exists.";
+        }
+    }
+}
