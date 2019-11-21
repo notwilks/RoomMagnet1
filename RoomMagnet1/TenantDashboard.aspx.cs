57648680ad72d04e6ca979f581304d80f518d7c8
Wyatt Wilkinson <wilkinson1101@gmail.com>
2019-11-20 22:35:03 -0500

    -Added Validation

--- RoomMagnet1/TenantDashboard.aspx.cs
+++ RoomMagnet1/TenantDashboard.aspx.cs
@@ -14,75 +14,80 @@ public partial class TenantDashboard : System.Web.UI.Page
     SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["RoomMagnetAWS"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
-
-        // Load database data into local variables to be displayed on dashboard
-        sc.Open();
-        SqlCommand selectTenantInfo = new SqlCommand("SELECT concat(firstName, ' ', lastName), email," +
-            "phoneNumber, birthDate, isnull(biography, 'bio goes here'), cleared FROM [Tenant] where email = @email", sc);
-        selectTenantInfo.Parameters.AddWithValue("@email", Session["userEmail"]);
-        SqlDataReader reader = selectTenantInfo.ExecuteReader();
-        String name = "";
-        String email = "";
-        String phoneNumber = "";
-        DateTime DOB = DateTime.Now;
-        DateTime today = DateTime.Now;
-        String age = "";
-        String bio = "";
-        char badge1 = 'F';
-        char badge2 = 'F';
-        while (reader.Read())
+        if (Convert.ToString(Session["userEmail"]).Equals(""))
         {
-            name = reader.GetString(0);
-            email = reader.GetString(1);
-            phoneNumber = reader.GetString(2);
-            DOB = Convert.ToDateTime(reader.GetDateTime(3));
-            today = DateTime.Now;
-            age = CalculateAge(reader.GetDateTime(3)).ToString();
-            bio = reader.GetString(4);
-
-            String cleared = reader.GetString(5);
-            if (cleared == "T")
-            {
-                TenantBackgroundStatusImage.ImageUrl = "images/icons-07.png";
-                TenantBackgroundStatusDescrip.Text = "You are a verified user! Your background check has been completed and you are cleared.";
-                TenantBackgroundStatusWords.Text = "Completed";
-            }
-            else
-            {
-                TenantBackgroundStatusImage.ImageUrl = "images/icons-08.png";
-                TenantBackgroundStatusDescrip.Text = "Your background check has either not yet been submitted or is currently under review.";
-                TenantBackgroundStatusWords.Text = "Not Completed";
-            }
+            Response.Redirect("HomePage.aspx");
         }
-        reader.Close();
-
-        SqlCommand selectBadge = new SqlCommand("SELECT undergraduate, graduate FROM TenantBadge where tenantID = (select tenantID from Tenant where email = @email2)", sc);
-        selectBadge.Parameters.AddWithValue("@email2", Session["userEmail"]);
-        reader = selectBadge.ExecuteReader();
-        while (reader.Read())
+        else
         {
-            badge1 = Convert.ToChar(reader["undergraduateBadge"]);
-            badge2 = Convert.ToChar(reader["graduateBadge"]);
-        }
-        reader.Close();
+            // Load database data into local variables to be displayed on dashboard
+            sc.Open();
+            SqlCommand selectTenantInfo = new SqlCommand("SELECT concat(firstName, ' ', lastName), email," +
+                "phoneNumber, birthDate, isnull(biography, 'bio goes here'), cleared FROM [Tenant] where email = @email", sc);
+            selectTenantInfo.Parameters.AddWithValue("@email", Session["userEmail"]);
+            SqlDataReader reader = selectTenantInfo.ExecuteReader();
+            String name = "";
+            String email = "";
+            String phoneNumber = "";
+            DateTime DOB = DateTime.Now;
+            DateTime today = DateTime.Now;
+            String age = "";
+            String bio = "";
+            char badge1 = 'F';
+            char badge2 = 'F';
+            while (reader.Read())
+            {
+                name = reader.GetString(0);
+                email = reader.GetString(1);
+                phoneNumber = reader.GetString(2);
+                DOB = Convert.ToDateTime(reader.GetDateTime(3));
+                today = DateTime.Now;
+                age = CalculateAge(reader.GetDateTime(3)).ToString();
+                bio = reader.GetString(4);
 
-        string image1 = checkBadge(badge1, "images/undergrad-badge.png");
-        string image2 = checkBadge(badge2, "images/masters-badge.png");
+                String cleared = reader.GetString(5);
+                if (cleared == "T")
+                {
+                    TenantBackgroundStatusImage.ImageUrl = "images/icons-07.png";
+                    TenantBackgroundStatusDescrip.Text = "You are a verified user! Your background check has been completed and you are cleared.";
+                    TenantBackgroundStatusWords.Text = "Completed";
+                }
+                else
+                {
+                    TenantBackgroundStatusImage.ImageUrl = "images/icons-08.png";
+                    TenantBackgroundStatusDescrip.Text = "Your background check has either not yet been submitted or is currently under review.";
+                    TenantBackgroundStatusWords.Text = "Not Completed";
+                }
+            }
+            reader.Close();
 
-        FirstNameLastNameHeader.Text = HttpUtility.HtmlEncode(name) + "'s Dashboard";
-        FirstNameLastNameAge.Text = HttpUtility.HtmlEncode(name) + ", " + age;
-        BioLabel.Text = HttpUtility.HtmlEncode(bio);
+            SqlCommand selectBadge = new SqlCommand("SELECT undergraduate, graduate FROM TenantBadge where tenantID = (select tenantID from Tenant where email = @email2)", sc);
+            selectBadge.Parameters.AddWithValue("@email2", Session["userEmail"]);
+            reader = selectBadge.ExecuteReader();
+            while (reader.Read())
+            {
+                badge1 = Convert.ToChar(reader["undergraduateBadge"]);
+                badge2 = Convert.ToChar(reader["graduateBadge"]);
+            }
+            reader.Close();
 
-        SqlCommand selectImages = new SqlCommand();
-        selectImages.Connection = sc;
+            string image1 = checkBadge(badge1, "images/undergrad-badge.png");
+            string image2 = checkBadge(badge2, "images/masters-badge.png");
 
-        selectImages.CommandText = "Select tenantID from Tenant where email = @tenEmail";
-        selectImages.Parameters.AddWithValue("@tenEmail", Session["userEmail"]);
+            FirstNameLastNameHeader.Text = HttpUtility.HtmlEncode(name) + "'s Dashboard";
+            FirstNameLastNameAge.Text = HttpUtility.HtmlEncode(name) + ", " + age;
+            BioLabel.Text = HttpUtility.HtmlEncode(bio);
 
-        int tenantID = Convert.ToInt32(selectImages.ExecuteScalar());
+            SqlCommand selectImages = new SqlCommand();
+            selectImages.Connection = sc;
 
-        selectImages.CommandText = "Select ISNULL(mainImage, ''), ISNULL(image2, ''), ISNULL(image3, '') FROM TenantImages where tenantID = " + tenantID;
-        reader = selectImages.ExecuteReader();
+            selectImages.CommandText = "Select tenantID from Tenant where email = @tenEmail";
+            selectImages.Parameters.AddWithValue("@tenEmail", Session["userEmail"]);
+
+            int tenantID = Convert.ToInt32(selectImages.ExecuteScalar());
+
+            selectImages.CommandText = "Select ISNULL(mainImage, ''), ISNULL(image2, ''), ISNULL(image3, '') FROM TenantImages where tenantID = " + tenantID;
+            reader = selectImages.ExecuteReader();
 
             while (reader.Read())
             {
@@ -93,86 +98,86 @@ public partial class TenantDashboard : System.Web.UI.Page
             reader.Close();
 
 
-        //Header.Text = "Host Dashboard.";
-        //select.CommandText = "Select (firstName + ' ' + lastName) from host where email = @email1";
-        //select.Parameters.Add(new System.Data.SqlClient.SqlParameter("@email1", Session["userEmail"]));
-        //String hostName = Convert.ToString(select.ExecuteScalar());
-        //ProfileHeader.Text = "Welcome " + hostName;
+            //Header.Text = "Host Dashboard.";
+            //select.CommandText = "Select (firstName + ' ' + lastName) from host where email = @email1";
+            //select.Parameters.Add(new System.Data.SqlClient.SqlParameter("@email1", Session["userEmail"]));
+            //String hostName = Convert.ToString(select.ExecuteScalar());
+            //ProfileHeader.Text = "Welcome " + hostName;
 
-        SqlCommand select = new SqlCommand();
-        select.Connection = sc;
+            SqlCommand select = new SqlCommand();
+            select.Connection = sc;
 
-        select.CommandText = "select a.accommodationID, a.description, h.firstName, h.cleared from FavoriteProperty fp " +
-            "inner join Accommodation a on a.accommodationID = fp.accommodationID " +
-            "inner join Host h on h.hostID = a.hostID where fp.tenantID = " + tenantID;
+            select.CommandText = "select a.accommodationID, a.description, h.firstName, h.cleared from FavoriteProperty fp " +
+                "inner join Accommodation a on a.accommodationID = fp.accommodationID " +
+                "inner join Host h on h.hostID = a.hostID where fp.tenantID = " + tenantID;
 
-        reader = select.ExecuteReader();
+            reader = select.ExecuteReader();
 
-        while (reader.Read())
-        {
-            //Generating the initial div
-            var div1 = new HtmlGenericControl("div")
+            while (reader.Read())
             {
+                //Generating the initial div
+                var div1 = new HtmlGenericControl("div")
+                {
 
-            };
-            div1.Attributes.Add("class", "col-md-6");
-            //div1.Style.Add("margin-top", "1rem;");
-            div1.Style.Add("border-top", "solid");
-            div1.Style.Add("border-top-width", "1px");
-            div1.Style.Add("border-bottom", "solid");
-            div1.Style.Add("border-bottom-width", "1px");
-            favorites.Controls.Add(div1);
+                };
+                div1.Attributes.Add("class", "col-md-6");
+                //div1.Style.Add("margin-top", "1rem;");
+                div1.Style.Add("border-top", "solid");
+                div1.Style.Add("border-top-width", "1px");
+                div1.Style.Add("border-bottom", "solid");
+                div1.Style.Add("border-bottom-width", "1px");
+                favorites.Controls.Add(div1);
 
-            var hostName = new HtmlGenericControl("h4")
-            {
+                var hostName = new HtmlGenericControl("h4")
+                {
 
-            };
-            hostName.InnerText = reader.GetString(2) + "'s Property";
-            div1.Controls.Add(hostName);
+                };
+                hostName.InnerText = reader.GetString(2) + "'s Property";
+                div1.Controls.Add(hostName);
 
-            var propDesc = new HtmlGenericControl("h6")
-            {
+                var propDesc = new HtmlGenericControl("h6")
+                {
 
-            };
-            propDesc.InnerText = reader.GetString(1);
-            div1.Controls.Add(propDesc);
+                };
+                propDesc.InnerText = reader.GetString(1);
+                div1.Controls.Add(propDesc);
 
-            var div2 = new HtmlGenericControl("div")
-            {
+                var div2 = new HtmlGenericControl("div")
+                {
 
-            };
-            favorites.Controls.Add(div2);
-            div2.Attributes.Add("class", "col-md-6");
-            div2.Style.Add("border-top", "solid");
-            div2.Style.Add("border-top-width", "1px");
-            div2.Style.Add("border-bottom", "solid");
-            div2.Style.Add("border-bottom-width", "1px");
-            //div2.Style.Add("margin-top", "1rem;");
+                };
+                favorites.Controls.Add(div2);
+                div2.Attributes.Add("class", "col-md-6");
+                div2.Style.Add("border-top", "solid");
+                div2.Style.Add("border-top-width", "1px");
+                div2.Style.Add("border-bottom", "solid");
+                div2.Style.Add("border-bottom-width", "1px");
+                //div2.Style.Add("margin-top", "1rem;");
 
-            Image status = new Image();
-            String cleared = reader.GetString(3);
-            if (cleared == "T")
-            {
-                status.ImageUrl = "images/icons-07.png";
+                Image status = new Image();
+                String cleared = reader.GetString(3);
+                if (cleared == "T")
+                {
+                    status.ImageUrl = "images/icons-07.png";
+                }
+                else
+                {
+                    status.ImageUrl = "images/icons-08.png";
+                }
+                status.Style.Add("max-width", "50px");
+                div2.Controls.Add(status);
+
+                Button viewProf = new Button();
+                viewProf.ID = Convert.ToString(reader.GetInt32(0));
+                viewProf.Text = "View Property";
+                viewProf.Attributes.Add("class", "btn");
+                viewProf.Style.Add("margin-left", "1rem");
+                viewProf.Click += new EventHandler(ViewProfile_Click);
+                div2.Controls.Add(viewProf);
+
+
             }
-            else
-            {
-                status.ImageUrl = "images/icons-08.png";
-            }
-            status.Style.Add("max-width", "50px");
-            div2.Controls.Add(status);
-
-            Button viewProf = new Button();
-            viewProf.ID = Convert.ToString(reader.GetInt32(0));
-            viewProf.Text = "View Property";
-            viewProf.Attributes.Add("class", "btn");
-            viewProf.Style.Add("margin-left", "1rem");
-            viewProf.Click += new EventHandler(ViewProfile_Click);
-            div2.Controls.Add(viewProf);
-
-
         }
-
     }
     protected void EditProfileBtn_Click(object sender, EventArgs e)
     {
